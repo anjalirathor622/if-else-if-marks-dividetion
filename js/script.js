c77f642c93ccd0b577e6f8d17b2b4b79adddcb6d
anjalirathor622 <anjalirathor622@gmail.com>
2022-12-10 10:11:32 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-let per = prompt("enter your percentage");
 
-let elem = document.getElementById("percent");
-if(per >= 80 && per <= 100){
+// var variable
+//promt box 
+var per = prompt("enter your percentage");  
+
+let elem = document.getElementById("percent");  
+
+ // if else if 
+if(per >= 80 && per <= 100){    //&& logical or opretor
+
     elem.innerHTML = "congrats!!! you are in merit.";
+    elem.style.color = 'green'
 }else if(per >= 60 && per < 80){
+
     elem.innerHTML = "congrats!!! you are in 1st division.";
+    elem.style.color = 'blue'
 }else if(per >= 45 && per < 60){
+
     elem.innerHTML = "congrats!!! you are in 2nd division.";
+    elem.style.color = 'orange'
 }else if(per >= 33 && per < 45){
+
     elem.innerHTML = "congrats!!! you are in 3rd division.";
+    elem.style.color = 'yellow'
 }else if(per < 33){
+
     elem.innerHTML = "sorry!!! you are fail.";
+    elem.style.color = 'red'
 }else{
     alert('please enter valid percent');
 }
